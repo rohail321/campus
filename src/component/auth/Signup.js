7b9ca7e0e44429fed5e420f64eef66896b69f25a
rohail321 <qrohail32@gmail.com>
2020-03-22 03:14:09 +0500

    second commit

--- src/component/auth/Signup.js
+++ src/component/auth/Signup.js
@@ -68,22 +68,7 @@ export class Signup extends Component {
             
         })
 
-        // var actionCodeSettings = {
-        //     // URL you want to redirect back to. The domain (www.example.com) for this
-        //     // URL must be whitelisted in the Firebase Console.
-        //     url: 'http://localhost:3000',
-        //     // This must be true.
-        //     handleCodeInApp: true,
-            
-
-          
-        //   };
-        // firebase.auth().sendSignInLinkToEmail(this.state.email,actionCodeSettings)
-        // .then((res)=>{
-        //     window.localStorage.setItem('emailForSignIn', this.state.email);
-        //     console.log(res)
-        //     alert('Email have been send to your email account please verify it!')
-        // })
+        
         console.log(this.state)
 }
 
@@ -120,43 +105,7 @@ export class Signup extends Component {
 
 
       
-      {/* <fieldset>
-        <legend><span class="number">2</span>Your profile</legend>
-        <label for="bio">Biography:</label>
-        <textarea id="bio" name="user_bio"></textarea>
-      </fieldset>
-      <fieldset>
-      <label for="job">Job Role:</label>
-      <select id="job" name="user_job">
-        <optgroup label="Web">
-          <option value="frontend_developer">Front-End Developer</option>
-          <option value="php_developor">PHP Developer</option>
-          <option value="python_developer">Python Developer</option>
-          <option value="rails_developer"> Rails Developer</option>
-          <option value="web_designer">Web Designer</option>
-          <option value="WordPress_developer">WordPress Developer</option>
-        </optgroup>
-        <optgroup label="Mobile">
-          <option value="Android_developer">Androild Developer</option>
-          <option value="iOS_developer">iOS Developer</option>
-          <option value="mobile_designer">Mobile Designer</option>
-        </optgroup>
-        <optgroup label="Business">
-          <option value="business_owner">Business Owner</option>
-          <option value="freelancer">Freelancer</option>
-        </optgroup>
-        <optgroup label="Other">
-          <option value="secretary">Secretary</option>
-          <option value="maintenance">Maintenance</option>
-        </optgroup>
-      </select>
       
-        <label>Interests:</label>
-        <input type="checkbox" id="development" value="interest_development" name="user_interest"/><label class="light" for="development">Development</label><br/>
-          <input type="checkbox" id="design" value="interest_design" name="user_interest"/><label class="light" for="design">Design</label><br/>
-        <input type="checkbox" id="business" value="interest_business" name="user_interest"/><label class="light" for="business">Business</label>
-      
-      </fieldset> */}
     </form>
             </div>
         )
